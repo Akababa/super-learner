ce395d62ac090dff332c5bb3c7c47ab714fd6f48
lvlc <richhuhu@gmail.com>
2019-06-17 19:46:57 -0400

    models + meta

--- main.py
+++ main.py
@@ -15,16 +15,18 @@ svm2 = svm.SVR(kernel='poly', C=10, gamma='auto')
 rf = ensemble.RandomForestRegressor(n_estimators=20, random_state=1)
 model_lib = [ols, rf ,elnet, ridge, lars, lasso, nn, svm1, svm2]
 model_names = ["OLS", "RF", "ElasticNet", "Ridge", "LARS", "LASSO", "kNN", "SVM rbf", "SVM poly"]
-
+meta_learner = ols
 iris = datasets.load_iris()
 
 X_train, X_test, y_train, y_test = train_test_split(
     iris.data, iris.target, test_size=0.4, random_state=0)
 
 # clf = svm.SVC(kernel='linear', C=1).fit(X_train, y_train)
-clf = SuperLearner().fit(X_train, y_train)
+clf = SuperLearner(cand_learners=model_lib[0:2],
+                  meta_learner=meta_learner).fit(X_train, y_train)
 print(clf.score(X_test, y_test))
 from sklearn.model_selection import cross_val_score
 
 scores = cross_val_score(clf, iris.data, iris.target, cv=5)
 print(scores)
+
