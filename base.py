fixed underflow/overflow with Bayes factors

base.py
@@ -103,7 +103,7 @@ class BMA(BaseEstimator, RegressorMixin):
             ll_val = -n * np.log(2 * np.pi * var_eps) / 2 - n / 2 # simplified log likelihood
             BIC[i] = 2*ll_val + (p + 2) * np.log(n)
             # BIC[i] = np.log(n*mean_squared_error(y, cand.predict(X))) + (p+2)*np.log(n)
-
+        BIC = BIC - np.min(BIC)
         self.weights_ = np.exp(-0.5 * BIC) / (sum(np.exp(-0.5 * BIC))) # weighting formula
         return self
 